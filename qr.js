937e034b7cf2eb69ae3e5067760f5c7772121a69
Ashish Jangid <ashish.jangid@codebird.in>
2023-09-22 14:01:22 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -7,37 +7,41 @@ const Link = require('./models/qr');
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        const con = await mongoose.connect('mongodb+srv://ashishjangid:wZ0gmfpnyfZY9Sux@cluster0.yeraifz.mongodb.net/qrlinks', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log(`MongoDB connected : ${con.connection.host}`);
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
+  try {
+    const con = await mongoose.connect('mongodb+srv://ashishjangid:wZ0gmfpnyfZY9Sux@cluster0.yeraifz.mongodb.net/qrlinks', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log(`MongoDB connected : ${con.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 }
-connectDB();   
+connectDB();
 
-app.get("/api", (req, res) => {
-  (async () => {
+app.get("/api", async (req, res) => {
+  const record = async () => {
     // Generate the QR code
     const qrId = new mongoose.mongo.ObjectId()
     const data = new Link({
-        name: 'google_qr.png',
-        qrId: qrId
+      name: 'google_qr.png',
+      qrId: qrId
     })
     const saved = await data.save();
     if (saved._id) {
-        const redirectionLink = `https://www.google.com/${qrId}`;
-        QRCode.toFile('google_qr.png', redirectionLink, (err) => {
-            if (err) throw err;
-            console.log('QR code saved as google_qr.png');
-        });
+      const redirectionLink = `https://www.google.com/${qrId}`;
+      QRCode.toFile('google_qr.png', redirectionLink, (err) => {
+        if (err) {
+          return { data: err }
+        }else {
+          return { data: 'QR code saved as google_qr.png' };
+        }
+      });
     }
-})();
-  res.status(200).send({ success: true, message: "API Is running", errors: "" })
+  }
+  const resultl = await record();
+  res.status(200).send({ success: true, message: "API Is running", data: resultl, errors: "" })
 });
 
 
