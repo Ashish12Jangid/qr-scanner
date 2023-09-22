85d9921b81d0925203f18990b16545c476e5b008
Ashish Jangid <ashish.jangid@codebird.in>
2023-09-22 14:19:01 +0530

    Update qr.js

--- models/qr.js
+++ models/qr.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const linkSchema = new mongoose.Schema({
     name: String,
     qrId: mongoose.Types.ObjectId,
+    link: String
 });
 
 module.exports = mongoose.model('Link', linkSchema);
